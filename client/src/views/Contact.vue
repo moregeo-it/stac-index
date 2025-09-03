61f346ce61845900d7f80618d9d7bd726df6a41a
Matthias Mohr <webmaster@mamo-net.de>
2025-09-03 15:55:59 +0200

    Update imprint

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -2,19 +2,33 @@
   <b-container class="content contact">
     <h1>Contact</h1>
     <p>
-      This is a project to promote the STAC specification. It is operated by:
+      This is a project to promote the STAC specification.
+      For bug reports and suggestions you can go to the
+      <a href="https://github.com/stac-utils/stac-index/issues" target="_blank">project's issue tracker on GitHub</a>.
     </p>
     <p>
-      moreGeo GmbH<br />
+      This project is operated by:
+    </p>
+    <p>
+      <strong>moreGeo GmbH</strong><br />
       Wienburgstr. 207<br />
       48159 Münster<br />
       Germany
     </p>
     <p>
-      You can contact me via e-mail at mail@moregeo.it<br />
-      For bugs and suggestions you can refer to the <a href="https://github.com/stac-utils/stac-index/issues" target="_blank">project's issue tracker on GitHub</a>.
+      <strong>Managing director</strong><br />
+      Matthias Mohr
     </p>
-    <p>For complete contact and company details please check the <a href="https://moregeo.it/imprint.html" target="_blank">imprint</a> of moregeo.it.</p>
+    <p>
+      <strong>Contact</strong><br />
+      Email: m.mohr@moregeo.it<br />
+      Phone: +49 251 2039 8725 (first contact by email, please!)
+    </p>
+    <p>
+      <strong>Commercial Register Entry</strong><br />
+      Register court: Local Court of Münster<br />
+      Registration Number: HRB 22852
+    </p>
   </b-container>
 </template>
 
